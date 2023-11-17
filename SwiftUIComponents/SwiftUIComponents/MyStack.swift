a6e3419ad95cdd92feb35518396c5d3476af0763
wooseob <wooseob@ahope.co.kr>
2023-11-17 15:49:16 +0900

    Stack

---
+++ SwiftUIComponents/SwiftUIComponents/MyStack.swift
@@ -0,0 +1,37 @@
+//
+//  MyStack.swift
+//  SwiftUIComponents
+//
+//  Created by wooseob on 11/17/23.
+//
+
+import SwiftUI
+
+struct MyStack: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            VStack {
+                Text("1")
+                Text("2")
+                Text("3")
+                HStack {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+            }
+            .frame(width: 200, height: 300, alignment: .leading)
+            .background(.orange)
+        }
+       
+    }
+}
+
+#Preview {
+    MyStack()
+}
+// VStack은 10개밖에 못넣음
+// VStack은 수직으로 쌓을 수 있음
+// HStack은 수평으로 쌓을 수 있음
+// ZStack은 겹칠 수 있음
