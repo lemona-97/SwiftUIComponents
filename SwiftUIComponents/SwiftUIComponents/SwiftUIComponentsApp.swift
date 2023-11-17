ee7f6e04cd5e8b3bfce559d2a5b63f27d19c9414
wooseob <wooseob@ahope.co.kr>
2023-11-17 14:42:44 +0900

    initial commit

---
+++ SwiftUIComponents/SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by wooseob on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
